e33f55b20aefe158ec2d208f166241ee1d050447
Hanna Lee <mani21na@gmail.com>
2019-12-16 14:05:50 -0800

    first commit

---
+++ lib/daycare_organizer.rb
@@ -0,0 +1,6 @@
+require "daycare_organizer/version"
+
+module DaycareOrganizer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/daycare_organizer/version.rb
@@ -0,0 +1,3 @@
+module DaycareOrganizer
+  VERSION = "0.1.0"
+end
